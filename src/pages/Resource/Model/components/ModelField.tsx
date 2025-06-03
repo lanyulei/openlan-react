965a725c53acc0471c299e377c7dce20eb807a48
lanyulei <fdevops@163.com>
2025-06-03 21:45:00 +0800

    feat: 添加字段管理抽屉表单功能
    - 新增字段管理抽屉表单组件，支持创建和编辑字段
    - 添加状态管理控制抽屉显示和表单数据
    - 更新按钮图标和交互逻辑

--- src/pages/Resource/Model/components/ModelField.tsx
+++ src/pages/Resource/Model/components/ModelField.tsx
@@ -1,16 +1,58 @@
-import { SearchOutlined } from '@ant-design/icons';
+import { EyeOutlined, PlusOutlined, SearchOutlined } from '@ant-design/icons';
+import { DrawerForm } from '@ant-design/pro-components';
 import { Button, Flex, Input } from 'antd';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+
+interface FieldGroupForm {
+  name?: string;
+}
 
 const ModelField: FC = () => {
+  const [fieldGroupForm, setFieldGroupForm] = useState<FieldGroupForm>();
+  const [fieldVisit, setFieldVisit] = useState(false);
+  const [drawerStatus, setDrawerStatus] = useState('create');
+
   return (
     <div>
       <Flex gap={10} wrap>
-        <Button type="primary">新建字段</Button>
-        <Button>新建分组</Button>
-        <Button>字段预览</Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            setDrawerStatus('create');
+            setFieldVisit(true);
+          }}
+        >
+          <PlusOutlined />
+          新建字段
+        </Button>
+        <Button>
+          <PlusOutlined />
+          新建分组
+        </Button>
+        <Button>
+          <EyeOutlined />
+          字段预览
+        </Button>
         <Input placeholder="请输入字段名" style={{ width: '300px' }} suffix={<SearchOutlined />} />
       </Flex>
+      <DrawerForm
+        title={drawerStatus === 'create' ? '新建字段' : '编辑字段'}
+        initialValues={fieldGroupForm}
+        width={600}
+        autoFocusFirstInput
+        drawerProps={{
+          destroyOnHidden: true,
+        }}
+        onFinish={async (values) => {
+          console.log(values);
+          return true;
+        }}
+        onValuesChange={(_, values) => {
+          setFieldGroupForm((prev) => ({ ...prev, ...values }));
+        }}
+        onOpenChange={setFieldVisit}
+        open={fieldVisit}
+      ></DrawerForm>
     </div>
   );
 };
