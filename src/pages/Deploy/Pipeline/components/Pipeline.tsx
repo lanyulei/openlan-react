7d8f3c9b3c669b43281a1d054033ed3074226b5d
lanyulei <fdevops@163.com>
2025-06-20 13:51:11 +0800

    feat: 添加流水线侧边栏和内容列表，支持新建阶段功能

--- src/pages/Deploy/Pipeline/components/Pipeline.tsx
+++ src/pages/Deploy/Pipeline/components/Pipeline.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from 'react';
 import styles from './Pipeline.less';
 import { Button } from 'antd';
+import { PlusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
 const Pipeline: FC = () => {
-  const [active, setActive] = useState('basic');
+  const [active, setActive] = useState('process');
 
   return (
     <>
@@ -49,7 +50,36 @@ const Pipeline: FC = () => {
       </div>
       <div className={styles.pipelineContent}>
         {active === 'basic' && <div>这里是基本信息内容</div>}
-        {active === 'process' && <div>这里是流程配置内容</div>}
+        {active === 'process' && (
+          <div style={{ display: 'flex', height: '100%' }}>
+            <div className={styles.pipelineSidebar}>
+              <div className={styles.pipelineTitle}>流水线源</div>
+              <div className={styles.pipelineSource}></div>
+              <div>
+                <Button
+                  shape="round"
+                  icon={<PlusOutlined />}
+                  style={{ width: '100%', color: '#595c62', fontSize: '14px' }}
+                  size="large"
+                >
+                  新建阶段
+                </Button>
+              </div>
+            </div>
+            <div className={styles.pipelineContentList}>
+              <div className={styles.pipelineGroup}>
+                <div className={styles.pipelineSplitline}>
+                  <div className={styles.pipelineButton}>
+                    <PlusCircleOutlined />
+                  </div>
+                </div>
+                <div className={`${styles.pipelineEditable} ${styles.pipelineTitle}`}>
+                  <div className={styles.pipelineGroupHeader}>暂无流程</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         {active === 'webhook' && <div>这里是触发设置内容</div>}
         {active === 'variable' && <div>这里是变量和缓存内容</div>}
       </div>
